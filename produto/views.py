0073a196b0547d86bef7977a16cd14d2dcfcc55c
Tonilson Dias <tonilsonpedrodias@gmail.com>
2025-05-22 01:34:40 +0100

    Criando a view detalhe

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -8,12 +9,17 @@ class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
-    paginate_by = 2
+    paginate_by = 3
     
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhar Produto')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
+    
+    
+
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
